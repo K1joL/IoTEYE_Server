d18d0f687b55eeb7c0c01f79f78a6660d8d14962
Bogdan Shults <kzk19@mail.ru>
2024-02-23 16:25:56 +0300

    User class moved to device Some class fixes

--- Headers/device.h
+++ Headers/device.h
@@ -4,9 +4,11 @@
 #include <cstdint>
 #include <chrono>
 #include <mutex>
-#include <functional>
+#include <functional.h>
 #include <thread>
-#include <iostream>
+#include <jwt-cpp/jwt.h>
+
+using ms = std::chrono::milliseconds;
 
 namespace ioteyeDevice
 {
@@ -16,35 +18,70 @@ namespace ioteyeDevice
         {
         public:
             StateTimer(std::function<void(uint8_t)> callback, std::mutex* mutex);
+            void threadLoop();
             void ping();
             void setPingFalse();
             bool wasPing();
             void stop();
+            void setDelays(ms outdatedDelay, ms offlineDelay);
+            ms getRemainingTime();
+            ~StateTimer(){}
+            void threadStarted();
+            void killThread();
+            bool isThreadAlive();
         private:
             bool isStopped();
         private:
             bool m_wasPing = false;
             std::mutex *m_changingMutex = nullptr;
-            std::chrono::milliseconds m_outdatedDelay = std::chrono::milliseconds(5);
-            std::chrono::milliseconds m_offlineDelay = std::chrono::milliseconds(15);
+            ms m_outdatedDelay = ms(500);
+            ms m_offlineDelay = ms(1000);
             std::chrono::high_resolution_clock::time_point m_start;
             std::function<void(uint8_t)> m_cbChangeState;
             bool m_isStopped = false;
+            bool m_threadAlive = false;
         };
 
     public:
         Device();
-        uint64_t getID() { ping(); return m_id; }
+        Device(uint16_t outdatedDelay, uint16_t offlineDelay, uint16_t maxPins);
+        void generateToken();
+        std::string getToken();
+        inline uint64_t getID() { return m_id; }
         uint8_t getState();
         void changeState(uint8_t state);
         void ping() { m_stateTimer->ping(); };
-        ~Device();
+        ~Device(); 
+
+        // Virtual pins interactions
+        int addPin(uint16_t pinNumber, const std::string &dataType, const std::string &defaultData);
+        int changePin(uint16_t pinNumber, const std::string &data);
+        int removePin(uint16_t pinNumber);
+        std::string getPin(uint16_t pinNumber);
+
     private:
         static uint64_t m_idSequence;
-        uint64_t m_id = 0;
+        uint64_t m_id;
+        std::string m_token;
         uint8_t m_state = OFFLINE;
         std::mutex *m_timerMutex = nullptr;
         StateTimer *m_stateTimer;
+
+        // Virtual pins data type IDs
+        enum ContainerID
+        {
+            INTID = 105,
+            DOUBLEID = 100,
+            STRINGID = 115
+        };
+
+        // Virtual pins data
+        std::unordered_map<uint8_t, uint8_t> m_pinsType;
+        std::unordered_map<uint8_t, int> m_intPins;
+        std::unordered_map<uint8_t, double> m_doublePins;
+        std::unordered_map<uint8_t, std::string> m_stringPins;
+        uint16_t m_pinsCounter = 0;
+        uint16_t m_maxPins = 255;
 
     public:
         enum STATES
--- Sources/device.cpp
+++ Sources/device.cpp
@@ -1,53 +1,65 @@
 #include "device.h"
 
+#ifdef IOTEYE_DEVICE_DEBUG
+#include "functional.h"
+using namespace iotDebug;
+#endif // !IOTEYE_DEVICE_DEBUG
+
 ioteyeDevice::Device::StateTimer::StateTimer(std::function<void(uint8_t)> callback, std::mutex* mutex)
 : 
 m_changingMutex(mutex),
 m_cbChangeState{callback}
 {
     std::thread([this]()
+                { threadLoop(); })
+        .detach();
+}
+
+// TODO: Rewrite loop fucntion with Finite-state machine
+void ioteyeDevice::Device::StateTimer::threadLoop()
+{   
+    threadStarted();
+    while (true)
     {
-        while (true) 
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        if (wasPing() || isStopped())
         {
-            std::this_thread::sleep_for(std::chrono::milliseconds(1));
-            if(wasPing())
+            setPingFalse();
+            m_start = std::chrono::high_resolution_clock::now();
+            while (std::chrono::high_resolution_clock::now() - m_start < m_outdatedDelay)
             {
-                setPingFalse();
-                m_start = std::chrono::high_resolution_clock::now();
-                while (std::chrono::high_resolution_clock::now() - m_start < m_outdatedDelay)
-                {
-                    std::this_thread::sleep_for(std::chrono::milliseconds(1));
-                    if (wasPing())
-                        break;
-                }
-                if (wasPing())
-                {
-                    m_cbChangeState(ioteyeDevice::Device::ONLINE);
-                    setPingFalse();
-                    continue;
-                }
-                // set state = outdated
-                m_cbChangeState(ioteyeDevice::Device::OUTDATED);
-                m_start = std::chrono::high_resolution_clock::now();
-                while (std::chrono::high_resolution_clock::now() - m_start < m_offlineDelay)
-                {
-                    std::this_thread::sleep_for(std::chrono::milliseconds(1));
-                    if (wasPing())
-                        break;
-                }
-                if (wasPing())
-                {
-                    m_cbChangeState(ioteyeDevice::Device::ONLINE);
-                    setPingFalse();
-                    continue;
-                }
-                // set state = offline
-                m_cbChangeState(ioteyeDevice::Device::OFFLINE);
+                std::this_thread::sleep_for(std::chrono::milliseconds(1));
+                if (wasPing() || isStopped())
+                    break;
             }
-            if(isStopped())
-                break;
+            if (wasPing())
+            {
+                m_cbChangeState(ioteyeDevice::Device::ONLINE);
+                setPingFalse();
+                continue;
+            }
+            // set state = outdated
+            m_cbChangeState(ioteyeDevice::Device::OUTDATED);
+            m_start = std::chrono::high_resolution_clock::now();
+            while (std::chrono::high_resolution_clock::now() - m_start < m_offlineDelay)
+            {
+                std::this_thread::sleep_for(std::chrono::milliseconds(1));
+                if (wasPing() || isStopped())
+                    break;
+            }
+            if (wasPing())
+            {
+                m_cbChangeState(ioteyeDevice::Device::ONLINE);
+                setPingFalse();
+                continue;
+            }
+            // set state = offline
+            m_cbChangeState(ioteyeDevice::Device::OFFLINE);
         }
-    } ).detach();
+        if (isStopped())
+            break;
+    }
+    killThread();
 }
 
 void ioteyeDevice::Device::StateTimer::ping()
@@ -67,8 +79,9 @@ void ioteyeDevice::Device::StateTimer::setPingFalse()
 
 bool ioteyeDevice::Device::StateTimer::wasPing()
 {
+    bool temp = false;
     m_changingMutex->lock();
-    bool temp = m_wasPing;
+    temp = m_wasPing;
     m_changingMutex->unlock();
     return temp;
 }
@@ -80,10 +93,51 @@ void ioteyeDevice::Device::StateTimer::stop()
     m_changingMutex->unlock();
 }
 
-bool ioteyeDevice::Device::StateTimer::isStopped()
+void ioteyeDevice::Device::StateTimer::setDelays(ms outdatedDelay, ms offlineDelay)
 {
     m_changingMutex->lock();
-    bool temp = m_isStopped;
+    m_outdatedDelay = outdatedDelay;
+    m_offlineDelay = offlineDelay;
+    m_changingMutex->unlock();
+}
+
+ms ioteyeDevice::Device::StateTimer::getRemainingTime()
+{
+    ms temp(0);
+    m_changingMutex->lock();
+    temp = ms((std::chrono::high_resolution_clock::now() - m_start).count());
+    m_changingMutex->unlock();
+    return temp;
+}
+
+void ioteyeDevice::Device::StateTimer::threadStarted()
+{
+    m_changingMutex->lock();
+    m_threadAlive = true;
+    m_changingMutex->unlock();
+}
+
+void ioteyeDevice::Device::StateTimer::killThread()
+{
+    m_changingMutex->lock();
+    m_threadAlive = false;
+    m_changingMutex->unlock();
+}
+
+bool ioteyeDevice::Device::StateTimer::isThreadAlive()
+{
+    bool temp = false;
+    m_changingMutex->lock();
+    temp = m_threadAlive;
+    m_changingMutex->unlock();
+    return temp;
+}
+
+bool ioteyeDevice::Device::StateTimer::isStopped()
+{
+    bool temp = false;
+    m_changingMutex->lock();
+    temp = m_isStopped;
     m_changingMutex->unlock();
     return temp;
 }
@@ -96,6 +150,28 @@ ioteyeDevice::Device::Device()
     m_idSequence++;
     m_timerMutex = new std::mutex();
     m_stateTimer = new StateTimer{std::bind(&Device::changeState, this, std::placeholders::_1), m_timerMutex};
+    generateToken();
+}
+
+ioteyeDevice::Device::Device(uint16_t outdatedDelay, uint16_t offlineDelay, uint16_t maxPins) : Device()
+{
+    m_stateTimer->setDelays(ms(outdatedDelay), ms(offlineDelay));
+    m_maxPins = maxPins;
+}
+
+void ioteyeDevice::Device::generateToken()
+{
+    
+    auto token = jwt::create()
+                     .set_type("JWS")
+                     .set_payload_claim("deviceID", jwt::claim(std::to_string(m_id)))
+                     .sign(jwt::algorithm::hs256{std::to_string(m_stateTimer->getRemainingTime().count()) + std::to_string(65 + m_idSequence % 26)});
+    m_token = token;
+}
+
+std::string ioteyeDevice::Device::getToken()
+{
+    return m_token;
 }
 
 uint8_t ioteyeDevice::Device::getState()
@@ -116,6 +192,124 @@ void ioteyeDevice::Device::changeState(uint8_t state)
 ioteyeDevice::Device::~Device()
 {
     m_stateTimer->stop();
+    while(m_stateTimer->isThreadAlive()) std::this_thread::sleep_for(std::chrono::milliseconds(1));
     delete m_timerMutex;
     delete m_stateTimer;
+}
+
+int ioteyeDevice::Device::addPin(uint16_t pinNumber, const std::string& dataType, 
+    const std::string& defaultData)
+{
+    if(m_pinsCounter == m_maxPins) // the number of pins must be less than m_maxPins
+    {
+#ifdef IOTEYE_DEVICE_DEBUG
+        debugMessage("the number of pins must be less than ");
+        debugMessage(m_maxPins); NEWLINE
+#endif // !IOTEYE_DEVICE_DEBUG
+        return 2;
+    }
+    if(dataType.empty()) // if data type not specified
+    {
+#ifdef IOTEYE_DEVICE_DEBUG
+        debugMessage("data type is not specified"); NEWLINE
+#endif // !IOTEYE_DEVICE_DEBUG
+        return 1;
+    }
+    if(m_pinsType.find(pinNumber) != m_pinsType.end()) // is pin exists
+    {
+#ifdef IOTEYE_DEVICE_DEBUG
+        debugMessage("pin exists"); NEWLINE
+#endif // !IOTEYE_DEVICE_DEBUG
+        return 3;
+    }
+
+    switch(std::tolower(dataType[0]))
+    {
+    case INTID:
+        m_intPins.emplace(pinNumber, std::stoi(defaultData));
+        break;
+    case DOUBLEID:
+        m_doublePins.emplace(pinNumber, std::stod(defaultData));
+        break;
+    case STRINGID:
+        m_stringPins.emplace(pinNumber, defaultData);
+        break;
+    default:
+        // Invalid data type
+        return 4;
+    }
+    m_pinsType.emplace(pinNumber, std::tolower(dataType[0]));
+    m_pinsCounter++;
+    
+    // OK
+    return 0;
+}
+
+int ioteyeDevice::Device::changePin(uint16_t pinNumber, const std::string& data)
+{
+    if(m_pinsType.find(pinNumber) == m_pinsType.end())
+        return 1;
+    
+    switch(m_pinsType.at(pinNumber))
+    {
+    case INTID:
+        m_intPins.at(pinNumber) = std::stoi(data);
+        break;
+    case DOUBLEID:
+        m_doublePins.at(pinNumber) = std::stod(data);
+        break;
+    case STRINGID:
+        m_stringPins.at(pinNumber) = data;
+        break;
+    default:
+        // Invalid data type
+        return 4;
+    }
+
+    return 0;
+}
+
+int ioteyeDevice::Device::removePin(uint16_t pinNumber)
+{
+    if(m_pinsType.find(pinNumber) == m_pinsType.end())
+        return 1;
+    
+    switch(m_pinsType.at(pinNumber))
+    {
+    case INTID:
+        m_intPins.erase(m_intPins.find(pinNumber), ++m_intPins.find(pinNumber));
+        break;
+    case DOUBLEID:
+        m_doublePins.erase(m_doublePins.find(pinNumber), ++m_doublePins.find(pinNumber));
+        break;
+    case STRINGID:
+        m_stringPins.erase(m_stringPins.find(pinNumber), ++m_stringPins.find(pinNumber));
+        break;
+    default:
+        // Invalid data type
+        return 4;
+    }
+    m_pinsType.erase(m_pinsType.find(pinNumber), ++m_pinsType.find(pinNumber));
+    m_pinsCounter--;
+    
+    return 0;
+}
+
+std::string ioteyeDevice::Device::getPin(uint16_t pinNumber)
+{
+    if(m_pinsType.find(pinNumber) == m_pinsType.end())
+        return std::string{""};
+    
+    switch(m_pinsType.at(pinNumber))
+    {
+    case INTID:
+        return std::to_string(m_intPins.at(pinNumber));
+    case DOUBLEID:
+        return std::to_string(m_doublePins.at(pinNumber));
+    case STRINGID:
+        return m_stringPins.at(pinNumber);
+    default:
+        // Invalid data type
+        return std::string{""};
+    }
 }
